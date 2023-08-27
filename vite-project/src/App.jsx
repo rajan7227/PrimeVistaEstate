439873cdea5dc79e2b37f25095501992ea0d4660
rajan7227 <114588581+rajan7227@users.noreply.github.com>
2023-08-27 14:36:30 +0100

    added header with context inside and some styling

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,18 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Header from './components/header/Header'
+import "./App.css";
+import Header from "./components/header/Header";
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
+    <div className="app">
       <Header />
-        
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
--- vite-project/src/components/header/Header.jsx
+++ vite-project/src/components/header/Header.jsx
@@ -1,9 +1,28 @@
 import "./header.scss";
 
 function Header() {
-  return <>
-  <p className="header__title">This is a Header </p>
-  </>;
+  return (
+    <>
+      <section className="header">
+        <div className="header__wrapper">
+          <h1 className="header__logo">PrimeVista</h1>
+          <nav className="header__nav">
+            <ul className="header__ul">
+              <li className="header__home">
+                <a href="/">home</a>
+              </li>
+              <li className="header__properties">
+                <a href="/">properties</a>
+              </li>
+              <li className="header__contact">
+                <a href="/">Contact us</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default Header;
