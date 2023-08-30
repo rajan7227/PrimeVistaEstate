7009c433d75db915f2b2c77f6ec4c51a327213d8
rajan7227 <114588581+rajan7227@users.noreply.github.com>
2023-08-30 11:24:35 +0100

    added minor styling in the single property page

--- vite-project/src/pages/property/Property.jsx
+++ vite-project/src/pages/property/Property.jsx
@@ -2,14 +2,29 @@ import Header from "../../components/header/Header";
 import houses from "../../data/houses.json";
 import { useParams } from "react-router-dom";
 import React, { useState } from "react";
+import './Property.scss';
+
+
 function Property() {
     const { id } = useParams();
     const selectedHouse = houses.find((house)=>house.id === id);
   return (
     <>
       <Header />
-      <h2>{selectedHouse.title}</h2>
-      <p>${selectedHouse.price}</p>
+      <div className="property-container">
+    <img className="property-image" src={selectedHouse.image} alt="Property Image"></img>
+    <div className="property-details">
+      <h1 className="property-title">{selectedHouse.title}</h1>
+      <p className="property-price">${selectedHouse.price}</p>
+      <p className="property-description">{selectedHouse.description}.</p>
+      <div className="property-info">
+        <span className="property-info-item">Bathrooms: {selectedHouse.facilities.bathrooms}</span>
+        <span className="property-info-item">Parking: 2</span>
+        <span className="property-info-item">Bedrooms: {selectedHouse.facilities.bedrooms}</span>
+      </div>
+      <p className="property-address">Address: {selectedHouse.address}</p>
+    </div>
+  </div>
     </>
   );
 }
