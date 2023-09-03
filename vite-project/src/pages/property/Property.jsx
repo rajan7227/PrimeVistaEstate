99e2d777880e4eecb152fb91deae66a8fcc96600
rajan7227 <114588581+rajan7227@users.noreply.github.com>
2023-09-03 16:23:06 +0100

    added map components

--- vite-project/src/pages/property/Property.jsx
+++ vite-project/src/pages/property/Property.jsx
@@ -1,13 +1,26 @@
 import Header from "../../components/header/Header";
+import Map from "../../components/map/map";
 import houses from "../../data/houses.json";
 import { useParams } from "react-router-dom";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './Property.scss';
+import axios from 'axios'
 
 
 function Property() {
     const { id } = useParams();
-    const selectedHouse = houses.find((house)=>house.id === id);
+    const [selectedHouse, setSelectedHouse] = useState([]);
+    useEffect(()=>{
+      axios.get(`http://localhost:9090/property/${id}`)
+      .then((response)=>{
+          setSelectedHouse(response.data);
+      })
+      .catch((error)=>{
+        console.error("error", error);
+      });
+    },[setSelectedHouse]);
+
+    //const selectedHouse = houses.find((house)=>house.id === id);
   return (
     <>
       <Header />
@@ -15,15 +28,20 @@ function Property() {
     <img className="property-image" src={selectedHouse.image} alt="Property Image"></img>
     <div className="property-details">
       <h1 className="property-title">{selectedHouse.title}</h1>
-      <p className="property-price">${selectedHouse.price}</p>
+      <p className="property-price">${selectedHouse.price?.toLocaleString()}</p>
       <p className="property-description">{selectedHouse.description}.</p>
       <div className="property-info">
-        <span className="property-info-item">Bathrooms: {selectedHouse.facilities.bathrooms}</span>
+        <span className="property-info-item">Bathrooms: {selectedHouse.facilities?.bathrooms}</span>
         <span className="property-info-item">Parking: 2</span>
-        <span className="property-info-item">Bedrooms: {selectedHouse.facilities.bedrooms}</span>
+        <span className="property-info-item">Bedrooms: {selectedHouse.facilities?.bedrooms}</span>
       </div>
       <p className="property-address">Address: {selectedHouse.address}</p>
     </div>
+    <Map
+              address={selectedHouse?.address}
+              city={selectedHouse?.city}
+              country={selectedHouse?.country}
+            />
   </div>
     </>
   );
--- vite-project/src/pages/propertys/Propertys.jsx
+++ vite-project/src/pages/propertys/Propertys.jsx
@@ -1,27 +1,53 @@
 import Header from "../../components/header/Header";
-import './Propertys.scss';
-import houses from '../../data/houses.json';
+import "./Propertys.scss";
+import houses from "../../data/houses.json";
 import { Link } from "react-router-dom";
+import SearchBar from "../../components/searchBar/SearchBar";
+import { AiFillHeart } from "react-icons/ai";
+import axios from 'axios'
+import { useEffect, useState } from "react";
+
+
+
 
 function Propertys() {
+  const [property, setProperty] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:9090/property")
+      .then((response) => {
+        setProperty(response.data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }, [setProperty]);
+
   return (
     <>
       <Header />
-      {houses.map((house,i) => (
+      <SearchBar className="propertys__search" />
+      {property.map((house, i) => (
         <section className="propertys" key={i}>
           <div className="propertys__wrapper">
             <Link to={`/property/${house.id}`}>
               <div className="propertys__container">
                 <div className="propertys__card">
-                  <img className="propertys__image" src={house.image} alt={house.title} />
+                  <img
+                    className="propertys__image"
+                    src={house.image}
+                    alt={house.title}
+                  />
                   <div className="propertys__details">
                     <div className="propertys__details-wrapper">
                       <div className="propertys__details-title">
                         <p className="propertys__details-add">Just Added</p>
+                        <AiFillHeart size={30} />
                         <p className="propertys__details-save">Save</p>
                       </div>
                       <div className="propertys__details-info">
-                        <h5 className="propertys__details-price">£{house.price}</h5>
+                        <h5 className="propertys__details-price">
+                          £{house.price}
+                        </h5>
                         <span className="propertys__details-spec">
                           <span>
                             <p>{house.facilities.bedrooms} Bedrooms</p>
@@ -31,7 +57,9 @@ function Propertys() {
                           </span>
                         </span>
                         <section className="propertys__details-section">
-                          <h6 className="propertys__details-name">{house.title}</h6>
+                          <h6 className="propertys__details-name">
+                            {house.title}
+                          </h6>
                           <p>{house.address}</p>
                           <p>Description</p>
                         </section>
